98afff1708c68a8c9871deb40b48c6b66b64407b
Phil Fluegel <pfluegelcx@gmail.com>
2016-05-09 19:10:05 -0500

    updated look

---
+++ js/app.js
@@ -0,0 +1,13 @@
+(function(){
+var calendarDiv = $("<div class='calender'><iframe src='https://calendar.google.com/calendar/embed?showTabs=0&amp;showCalendars=0&amp;height=600&amp;wkst=2&amp;bgcolor=%23FFFFFF&amp;src=d9eau9ssmquv1homev69k0a230%40group.calendar.google.com&amp;color=%23182C57&amp;ctz=America%2FChicago'></iframe></div>");
+										
+		$('body').append(calendarDiv);
+	
+	$('.upcoming').on('click',function(){
+		$('.calender').fadeIn(800);
+		});
+
+ 	$('.calender').on('click',function(){
+		$(this).fadeOut(800);
+		});
+})($);
